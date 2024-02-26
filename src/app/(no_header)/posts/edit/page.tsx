dce18e4c493fdc7cacfb67c17fbe2c2ec4eff007
nakamurau1 <fyuichi@gmail.com>
2024-02-26 14:16:07 +0900

    Add post edit page skeleton.

--- src/app/(no_header)/posts/edit/page.tsx
+++ src/app/(no_header)/posts/edit/page.tsx
@@ -1,5 +1,7 @@
 import PostEditPage from '@/components/PostEditPage'
 import { fetchPostById } from '@/app/_lib/data'
+import { Suspense } from 'react'
+import { PostEditPageSkeleton } from '@/app/_ui/skeltons'
 
 export default async function Page({
   searchParams
@@ -11,6 +13,15 @@ export default async function Page({
     return <PostEditPage />
   }
 
+  return (
+    <Suspense fallback={<PostEditPageSkeleton />}>
+      <PostEditPageWrapper postId={postId} />
+    </Suspense>
+  )
+}
+
+const PostEditPageWrapper = async ({ postId }: { postId: string }) => {
   const post = await fetchPostById(postId ?? '')
+
   return <PostEditPage post={post} />
 }
--- src/app/_ui/skeltons.tsx
+++ src/app/_ui/skeltons.tsx
@@ -1,3 +1,7 @@
+import { FaPenFancy, FaPlay, FaCheck } from 'react-icons/fa6'
+import { GoArrowLeft } from 'react-icons/go'
+import { cn } from '@/app/_lib/utils'
+
 // Loading animation
 const shimmer =
   'before:absolute before:inset-0 before:-translate-x-full before:animate-[shimmer_2s_infinite] before:bg-gradient-to-r before:from-transparent before:via-white/60 before:to-transparent'
@@ -25,6 +29,37 @@ export const PostsSkeleton = () => {
           </div>
         </article>
       ))}
+    </div>
+  )
+}
+
+export const PostEditPageSkeleton = () => {
+  return (
+    <div>
+      <header className="sticky top-0 border-b border-solid border-gray-100 z-50">
+        <div className="flex items-center justify-between h-16 px-10">
+          <GoArrowLeft className=" text-gray-500 text-2xl" />
+          <div className="flex items-center">
+            <div>
+              <span className="inline-flex justify-center items-center ml-3 bg-gray-200 rounded-full w-[106px] h-[36px]" />
+            </div>
+          </div>
+        </div>
+      </header>
+      <main className="flex-auto w-full max-w-4xl px-4 py-2 mx-auto">
+        <div className="flex flex-col justify-center p-6">
+          <span className="mb-5 h-9 w-[400px] bg-gray-200" />
+          <div className="flex w-full">
+            <div className="flex-1 w-full min-h-[800px] rounded-lg border-none outline-0 resize-none p-5 bg-gray-200 relative" />
+            <div className="max-w-20 min-w-20 ml-5">
+              <div
+                className="w-[76px] h-[36px] inline-flex rounded-full bg-gray-200 shadow-md p-1 relative
+              before:z-0 before:absolute before:w-9 before:rounded-full before:top-1 before:bottom-1  before:transition before:duration-200"
+              />
+            </div>
+          </div>
+        </div>
+      </main>
     </div>
   )
 }
