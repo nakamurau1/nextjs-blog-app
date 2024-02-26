118f8ffdf0c272c769b28ba258951b4b661ec694
nakamurau1 <fyuichi@gmail.com>
2024-02-26 20:08:33 +0900

    Add a link to the user profile page on the UserButton

--- src/components/UserButton.tsx
+++ src/components/UserButton.tsx
@@ -1,6 +1,7 @@
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/Avatar'
 import { Button } from '@/components/Button'
 import { auth } from '@/auth'
+import { fetchUserByEmail } from '@/app/_lib/data'
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -14,6 +15,8 @@ import Link from 'next/link'
 export default async function UserButton() {
   const session = await auth()
   if (!session?.user) return <SignIn />
+
+  const userByEmail = await fetchUserByEmail(session.user.email ?? '')
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
@@ -31,14 +34,16 @@ export default async function UserButton() {
       </DropdownMenuTrigger>
       <DropdownMenuContent className="w-56" align="end" forceMount>
         <DropdownMenuLabel className="font-normal">
-          <div className="flex flex-col space-y-1">
-            <p className="text-sm font-medium leading-none">
-              {session.user.name}
-            </p>
-            <p className="text-xs leading-none text-muted-foreground">
-              {session.user.email}
-            </p>
-          </div>
+          <Link href={`/${userByEmail?.id}`}>
+            <div className="flex flex-col space-y-1">
+              <p className="text-sm font-medium leading-none">
+                {session.user.name}
+              </p>
+              <p className="text-xs leading-none text-muted-foreground">
+                {session.user.email}
+              </p>
+            </div>
+          </Link>
         </DropdownMenuLabel>
         <DropdownMenuItem>
           <Link href="/dashboard" className="w-full text-center">
